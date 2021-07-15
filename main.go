a3c32578f2d67ef3ed8edb72b02dd0ce495b483b
Jatinder Singh <jatinrheen@yahoo.com>
2021-07-15 16:32:29 -0400

    Update WebMessage

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello AWS Team"}
+		res := &response{Message: "Hello AWSomers !!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
